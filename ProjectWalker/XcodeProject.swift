ef04890cdc0bbaaeed5809f3cec4d120cb1bfaf1
Simeon Leifer <simeon@droolingcat.com>
2019-04-01 05:10:44 -0700

    create convenience init to load from xcode project

--- ProjectWalker/XcodeProject.swift
+++ ProjectWalker/XcodeProject.swift
@@ -9,16 +9,56 @@
 import Foundation
 
 public class XcodeProject {
+    public var path: String
     public var archiveVersion: Int
     public var objectVersion: Int
     public var rootObject: String
     private(set) public var objects: [String: ProjectObject]
 
     public init() {
-        archiveVersion = 0
-        objectVersion = 0
-        rootObject = ""
-        objects = [:]
+        self.path = ""
+        self.archiveVersion = 0
+        self.objectVersion = 0
+        self.rootObject = ""
+        self.objects = [:]
+    }
+
+    public convenience init(contentsOf path: String) {
+        self.init()
+        self.path = NSString(string: path).appendingPathComponent("project.pbxproj")
+        let url = URL(fileURLWithPath: self.path)
+        let data = try! Data(contentsOf: url)
+        var format: PropertyListSerialization.PropertyListFormat = .xml
+        let plist: ProjectFileDictionary = try! PropertyListSerialization.propertyList(from: data, options: [.mutableContainersAndLeaves], format: &format) as? ProjectFileDictionary ?? [:]
+
+        for key in plist.keys.sorted() {
+            if key == "archiveVersion" {
+                if let value = plist.int(forKey: "archiveVersion") {
+                    self.archiveVersion = value
+                }
+            }
+            if key == "objectVersion" {
+                if let value = plist.int(forKey: "objectVersion") {
+                    self.objectVersion = value
+                }
+            }
+            if key == "objects" {
+                if let objects = plist.dictionary(forKey: "objects") {
+                    for objectKey in objects.keys.sorted() {
+                        if let entry = objects.dictionary(forKey: objectKey) {
+                            if let obj = ProjectObject.decode(from: entry) {
+                                self.add(object: obj, for: objectKey)
+                            }
+                        }
+                    }
+                }
+            }
+            if key == "rootObject" {
+                if let value = plist.string(forKey: "rootObject") {
+                    self.rootObject = value
+                }
+            }
+        }
     }
 
     public func add(object: ProjectObject, for key: String) {
--- TestTool/main.swift
+++ TestTool/main.swift
@@ -10,42 +10,8 @@ import Foundation
 
 import ProjectWalker
 
-let path = "/Users/simeon/Documents/Code/git-glide/git-glide.xcodeproj/project.pbxproj"
-let url = URL(fileURLWithPath: path)
-let data = try! Data(contentsOf: url)
-public var format: PropertyListSerialization.PropertyListFormat = .xml
-let plist: ProjectFileDictionary = try! PropertyListSerialization.propertyList(from: data, options: [.mutableContainersAndLeaves], format: &format) as? ProjectFileDictionary ?? [:]
-
-public var project: XcodeProject = XcodeProject()
-
-for key in plist.keys.sorted() {
-    if key == "archiveVersion" {
-        if let value = plist.int(forKey: "archiveVersion") {
-            project.archiveVersion = value
-        }
-    }
-    if key == "objectVersion" {
-        if let value = plist.int(forKey: "objectVersion") {
-            project.objectVersion = value
-        }
-    }
-    if key == "objects" {
-        if let objects = plist.dictionary(forKey: "objects") {
-            for objectKey in objects.keys.sorted() {
-                if let entry = objects.dictionary(forKey: objectKey) {
-                    if let obj = ProjectObject.decode(from: entry) {
-                        project.add(object: obj, for: objectKey)
-                    }
-                }
-            }
-        }
-    }
-    if key == "rootObject" {
-        if let value = plist.string(forKey: "rootObject") {
-            project.rootObject = value
-        }
-    }
-}
+let path = "/Users/simeon/Documents/Code/git-glide/git-glide.xcodeproj"
+public var project: XcodeProject = XcodeProject(contentsOf: path)
 
 project.dumpUnhandledTypes()
 if let proj = project.project() {
