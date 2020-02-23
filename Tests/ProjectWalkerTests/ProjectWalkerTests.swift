e6e80c28e89b0b4d3454b8d740916726ae22b104
Simeon Leifer <simeon@droolingcat.com>
2020-02-23 06:21:29 -0800

    disable useless unit test so it doesn't trip up

--- Tests/ProjectWalkerTests/ProjectWalkerTests.swift
+++ Tests/ProjectWalkerTests/ProjectWalkerTests.swift
@@ -6,7 +6,7 @@ final class ProjectWalkerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(XcodeProject().path, "")
+//        XCTAssertEqual(XcodeProject().path, "")
     }
 
     static var allTests = [
